af075fad4f72bd76f79b4f25430afb810ad537d8
francisco2016 <otrayama@gmail.com>
2016-07-27 10:25:15 +0200

    -0100 añadida un ArrayList en el mt bounce() para almacenar las bolas y un for para poder dar movimiento a todas las bolas.

--- BallDemo.java
+++ BallDemo.java
@@ -1,6 +1,6 @@
 import java.awt.Color;
 import java.util.Random;
-
+import java.util.ArrayList;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -24,40 +24,44 @@ public class BallDemo
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int numBolas)
     {
         int ground = 400;   // position of the ground line
-                myCanvas.setVisible(true);
+        myCanvas.setVisible(true);
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
-        
-        Random aleatorio = new Random(); //------------------------------------------------------------------- 0100
-        int posicionX = aleatorio.nextInt(80);
-        int posicionY = aleatorio.nextInt(300);
-        int radio = aleatorio.nextInt(50) +11;
-        
-        float RED = aleatorio.nextFloat();
-        float BLUE = aleatorio.nextFloat();
-        float GREEN = aleatorio.nextFloat();
-        
-        Color color = new Color(RED, BLUE, GREEN);
-        
-        // crate and show the balls
-        BouncingBall ball = new BouncingBall(posicionX, posicionY, radio, color, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
 
+        Random aleatorio = new Random(); //------------------------------------------------------------------- 0100
+        ArrayList<BouncingBall> bolas2 = new ArrayList<>();
+
+        for(int i = 0; i < numBolas; i ++){//para iterar el nº de numBolas, e ir creando las bolas totales.
+            int posicionX = aleatorio.nextInt(80);
+            int posicionY = aleatorio.nextInt(300);
+            int radio = aleatorio.nextInt(50) +11;
+
+            float RED = aleatorio.nextFloat();
+            float BLUE = aleatorio.nextFloat();
+            float GREEN = aleatorio.nextFloat();
+
+            Color color = new Color(RED, BLUE, GREEN);
+
+            // crate and show the balls
+            BouncingBall ball = new BouncingBall(posicionX, posicionY, radio, color, ground, myCanvas);
+            ball.draw();
+            bolas2.add(ball);//guardo cada bola creada en la colección bolas2.
+        }
         // make them bounce
         boolean finished =  false;
         while(!finished) {
             myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
+            
+            for(BouncingBall bolas3: bolas2){//recorro la colección bolas2 para poder dar movimiento a cada bola
+            bolas3.move();
             // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
+            if(bolas3.getXPosition() >= 550) {//si una de las bolas llega a esta posición, la animación se para.
                 finished = true;
             }
+        }
         }
     }
 }
